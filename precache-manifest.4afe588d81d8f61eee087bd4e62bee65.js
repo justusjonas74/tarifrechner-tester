e4f60da9e48715629c8a24afac01f00d97dcea81
Francis Doege <francis.doege@web.de>
2020-08-22 21:23:04 +0200

    Updates

---
+++ precache-manifest.4afe588d81d8f61eee087bd4e62bee65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d582cd5e47ed2642fed73f738cc8c335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "877d36c81655aebf1d96",
+    "url": "/static/css/main.41b080f8.chunk.css"
+  },
+  {
+    "revision": "195f803bc597948fdbb9",
+    "url": "/static/js/2.983433de.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.983433de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877d36c81655aebf1d96",
+    "url": "/static/js/main.84ae02de.chunk.js"
+  },
+  {
+    "revision": "c850f39f4ba24d10267b",
+    "url": "/static/js/runtime-main.c8ded85e.js"
+  }
+]);
