37e68816ca3bcc656ae782274245c65e9a6ffae3
Francis Doege <francis.doege@web.de>
2024-03-15 12:51:19 +0100

    Moved dvbjs to server

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 "use client"
 import App from "@/components/App";
-// import Image from 'next/image'
-// import styles from './page.module.css'
 
-import StopBox from "@/components/StopBox";
-import { IPoint, IStop } from "dvbjs";
 
 
 
 export default function Home() {
 
-  const handleNewSelectedStop = (stop: IPoint) => {
-    alert(stop.name)
-  }
+
   return (
     <App />
   )
--- lib/tarifrechnerAnfrage.ts
+++ lib/tarifrechnerAnfrage.ts
@@ -1,21 +1,26 @@
-import 'dotenv/config'
-import { ITrip } from 'dvbjs'
-import { EFA_ANFRAGE } from 'pkm-tarifrechner'
-import { TarifrechnerRequestConfig } from 'pkm-tarifrechner/build/src/trRequest'
+import "dotenv/config";
+import { ITrip } from "dvbjs";
+import { EFA_ANFRAGE } from "pkm-tarifrechner";
+import { TarifrechnerRequestConfig } from "pkm-tarifrechner/build/src/trRequest";
 
 export default function tarifrechnerAnfrage(trip: ITrip) {
-    const api_key = process.env.NEXT_PUBLIC_APIKEY
-    const api_key_header = api_key ? { 'X-Api-Key': process.env.NEXT_PUBLIC_APIKEY } : {}
-
-    const config: TarifrechnerRequestConfig = {
-        url: "tarifrechner/",
-        axiosConfig: {
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/vnd.pkm-tarifrechner-v7+json;charset=UTF-8',
-                ...api_key_header
-            }
-        }
-    }
-    return EFA_ANFRAGE.fromITrip(trip, config)
+  const api_key = process.env.NEXT_PUBLIC_APIKEY;
+  const api_key_header = api_key ? { "X-Api-Key": api_key } : {};
+  const bundleId = process.env.NEXT_PUBLIC_BUNDLEID;
+  const bundleId_header = bundleId
+    ? { "X-Ios-Bundle-Identifier": bundleId }
+    : {};
+  const config: TarifrechnerRequestConfig = {
+    url: "tarifrechner/",
+    axiosConfig: {
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/vnd.pkm-tarifrechner-v7+json;charset=UTF-8",
+        ...api_key_header,
+        ...bundleId_header,
+      },
+    },
+  };
+  console.log(config);
+  return EFA_ANFRAGE.fromITrip(trip, config);
 }
