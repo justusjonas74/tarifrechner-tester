d99b58c8162ab5b02d1dec89f67c1fefbc2bd486
Francis Doege <francis.doege@web.de>
2025-03-27 15:28:11 +0100

    mend

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 require("dotenv").config();
+const { join } = require("path");
 
+console.log(join(__dirname, "../tarifrechner-sst"));
 const nextConfig = {
   output: "standalone",
-  allowedDevOrigins: [process.env.NEXT_PUBLIC_URL],
+  allowedDevOrigins: [process.env.NEXT_PUBLIC_URL, "efm.lan.vvo"],
+  outputFileTracingRoot: join(__dirname, "../"),
   async rewrites() {
     return [
       {
