bdea87192150a430e1a1c591504197ab93f8ac13
Francis Doege <francis.doege@web.de>
2025-07-22 16:30:49 +0200

    Save previous Anfragen and use the on next request

--- components/CicoTestComponent.tsx
+++ components/CicoTestComponent.tsx
@@ -1,14 +1,16 @@
 import { ITrip } from "dvbjs";
 import { useState } from "react";
 import RoutingComponent from "./RoutingComponent";
-import TarifrechnerComponent from "./TarifrechnerComponent";
 import TarifrechnerCiCoComponent from "./TarifrechnerCICOComponent";
-import { IFAIRTIQ_REISENDER, } from "pkm-tarifrechner/build/src/tarifrechner/fairtiq/interfaces";
+import { IFAIRTIQ_ANFRAGE_ANTWORT, IFAIRTIQ_ANFRAGELISTE, IFAIRTIQ_ANTWORTLISTE, IFAIRTIQ_REISENDER, IFAIRTIQ_VERBINDUNG, } from "pkm-tarifrechner/build/src/tarifrechner/fairtiq/interfaces";
 import ReisendenComponent from "./ReisendenComponent";
+import PreviousRequestComponent from "./PreviousRequestComponent";
+import { toast } from "react-toastify";
 
 export default function CicoTestComponent() {
 
     const [selectedTrip, setSelectedTrip] = useState<ITrip | null>(null);
+    const [previousCiCoRequests, setPreviousCiCoRequests] = useState<IFAIRTIQ_ANFRAGE_ANTWORT[]>([]);
 
     const defaultReisender: IFAIRTIQ_REISENDER = {
         typ: {
@@ -30,17 +32,38 @@ export default function CicoTestComponent() {
     const handleChangedTrip = (trip: ITrip | null) => {
         setSelectedTrip(trip);
     };
+
+    const handleAddCiCoRequests = (request: IFAIRTIQ_ANFRAGE_ANTWORT) => {
+        // Check if the request already exists
+        const exists = previousCiCoRequests.some(
+            (prevRequest) =>
+                JSON.stringify(prevRequest.anfrage) === JSON.stringify(request.anfrage) &&
+                JSON.stringify(prevRequest.antwort) === JSON.stringify(request.antwort)
+        );
+        if (exists) {
+            toast.warn("Request already exists, not adding again.");
+            return;
+        }
+        // Add the new request to the list      
+        setPreviousCiCoRequests((prevRequests) => [...prevRequests, request]);
+        toast.success("FAIRTIQ-Antwort gespeichert.");
+    };
+
     return (
         <div className="container">
             <h1>Check-In/Check-Out-Tests</h1>
 
-            <h2 className="my-4">
-                {reisendenliste.length > 0 ? "Ausgewählte Reisende" : "Reisende auswählen"}
-            </h2>
-            <ReisendenComponent
-                reisendenliste={reisendenliste}
-                handleChangedReisendenliste={handleChangedReisendenliste}
-            />
+
+            <div className="row">
+                <ReisendenComponent
+                    reisendenliste={reisendenliste}
+                    handleChangedReisendenliste={handleChangedReisendenliste}
+                />
+                <PreviousRequestComponent
+                    previousRequests={previousCiCoRequests}
+                    handleResetPreviousCiCoRequests={() => setPreviousCiCoRequests([])}
+                />
+            </div>
             <h2 className="my-4">
                 {selectedTrip ? "Ausgewählte Verbindung" : "Verbindungsauswahl"}
             </h2>
@@ -53,7 +76,12 @@ export default function CicoTestComponent() {
             {selectedTrip && reisendenliste.length > 0 && (
                 <>
                     <h2 className="my-4">Tarifrechner-Antworten</h2>
-                    <TarifrechnerCiCoComponent selectedTrip={selectedTrip} reisendenliste={reisendenliste} />
+                    <TarifrechnerCiCoComponent
+                        previousCiCoRequests={previousCiCoRequests}
+                        selectedTrip={selectedTrip}
+                        reisendenliste={reisendenliste}
+                        handleSaveCiCoRequest={handleAddCiCoRequests}
+                    />
                 </>
             )}
         </div >
