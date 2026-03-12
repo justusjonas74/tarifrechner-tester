20fb9e303982e6917afc6644da1537e390ae6869
Francis Doege <francis.doege@web.de>
2026-03-12 13:04:45 +0100

    Setup Flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Next.js Entwicklungsumgebung mit pnpm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.nodePackages.pnpm
+          ];
+
+          shellHook = ''
+            echo "🚀 Flake-basierte Next.js Shell aktiv!"
+            # Optional: pnpm Store Pfad für Ubuntu :optimieren
+            export PNPM_HOME="$HOME/.local/share/pnpm"
+            export PATH="$PNPM_HOME:$PATH"
+          '';
+        };
+      });
+}
